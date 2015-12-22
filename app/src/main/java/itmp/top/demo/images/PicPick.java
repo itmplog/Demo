6c2c982623948a8b7df7598057c140a39e574526
Law Yu <Law Yu>
2015-12-22 23:50:52 +0800

    add permission

--- app/src/main/java/itmp/top/demo/images/PicPick.java
+++ app/src/main/java/itmp/top/demo/images/PicPick.java
@@ -1,21 +1,31 @@
 package itmp.top.demo.images;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.security.Permission;
 
 import itmp.top.demo.R;
 
 public class PicPick extends AppCompatActivity {
 
     private final int RESULT_LOAD_IMAGE = 0;
+    private final int RequestPermissionsResult = 1;
+    private Intent data_get = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,21 +50,51 @@ public class PicPick extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        //request permission
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
+            if(ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_CONTACTS)){
+                    Toast.makeText(getApplicationContext(), "i need read permisson", Toast.LENGTH_SHORT).show();
+                }else{
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            RequestPermissionsResult);
+                    data_get = data;
+                }
+            }
 
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
+        }
+    }
 
-            ImageView imageView = (ImageView) findViewById(R.id.imgView);
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case RequestPermissionsResult:
+                if(grantResults.length > 0) {
+                    Uri selectedImage = data_get.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
 
+                    Cursor cursor = getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    ImageView imageView = (ImageView) findViewById(R.id.imgView);
+                    imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                    break;
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "No Permission", Toast.LENGTH_SHORT).show();
+                }
+                default:
+                    break;
         }
     }
 }
