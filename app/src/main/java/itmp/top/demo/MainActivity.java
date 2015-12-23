d8640dfe6492481cee40a70595491572f294b21c
leehz <hz@huaixiaoz.com>
2015-12-23 12:55:18 +0800

    mv images to subMenu

--- app/src/main/java/itmp/top/demo/MainActivity.java
+++ app/src/main/java/itmp/top/demo/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 
 import itmp.top.demo.images.ImageGrid;
 import itmp.top.demo.images.PicPick;
@@ -21,8 +22,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         menu.add(0, 1, 0, getString(R.string.demo001_sendmessage)).setIcon(android.R.drawable.ic_menu_mapmode);
+        SubMenu subMenu =  menu.addSubMenu(0, 200, 0, getString(R.string.images)).setIcon(android.R.drawable.ic_menu_mapmode);
+        subMenu.add(1, 201, 0, getString(R.string.imagegrid));
+        subMenu.add(1, 202, 0, getString(R.string.picpick));
+        subMenu.add(1, 203, 0, getString(R.string.bitmaptest));
+        /* move to subMenu
         menu.add(0, 2, 0, getString(R.string.imagegrid)).setIcon(android.R.drawable.ic_menu_mapmode);
         menu.add(0, 3, 0, getString(R.string.picpick)).setIcon(android.R.drawable.ic_menu_mapmode);
+        */
         menu.add(0, 4, 0, "demo 004").setIcon(android.R.drawable.ic_menu_mapmode);
         return super.onCreateOptionsMenu(menu);
     }
