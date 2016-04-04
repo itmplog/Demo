8272ecb487554bc01da5551aacf6d3f6be8d9f80
huaixiaoz <hello@itmp.top>
2016-04-04 21:45:50 +0800

    swiperefresh

--- app/src/main/java/itmp/top/demo/services/DownloadActivity.java
+++ app/src/main/java/itmp/top/demo/services/DownloadActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.Iterator;
--- app/src/main/java/itmp/top/demo/services/ViewGroupTest.java
+++ app/src/main/java/itmp/top/demo/services/ViewGroupTest.java
@@ -1,7 +1,11 @@
 package itmp.top.demo.services;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+
 
 import itmp.top.demo.R;
 
@@ -9,9 +13,42 @@ import itmp.top.demo.R;
  * Created by hz on 2016/4/2.
  */
 public class ViewGroupTest extends AppCompatActivity {
+    SwipeRefreshLayout mSwipeRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_down_from_intent_service);
+
+        mSwipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.refresh);
+
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+//                super.handleMessage(msg);
+                if(msg.what == 0x111){
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+            }
+        };
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                synchronized (mSwipeRefreshLayout) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(5000);
+                                Message message = new Message();
+                                message.what = 0x111;
+                                handler.sendMessage(message);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+                }
+            }
+        });
     }
 }
